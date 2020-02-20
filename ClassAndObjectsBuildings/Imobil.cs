dfc8f7ee5ed81ad603391c46e5f7d4f07f8982a9
aurora <dragoiaurora92@gmail.com>
2020-02-20 21:44:34 +0200

    add building project

---
+++ ClassAndObjectsBuildings/Imobil.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassAndObjectsBuildings
+{
+    class Imobil
+    {
+        public string tipCasa { get; set; }
+        public int nrEtaje { get; set; }
+        public double pret { get; set; }
+
+        public Imobil()
+        {
+            tipCasa = "";
+            nrEtaje = 0;
+            pret = 0.00;
+        }
+        public Imobil(string type)
+        {
+            this.tipCasa = type;
+            this.nrEtaje = 0;
+            this.pret = 0.00;
+
+        }
+        public Imobil(string model, int floor, double price)
+        {
+            this.tipCasa = model;
+            this.nrEtaje = floor;
+            this.pret = price;
+        }
+    }
+}
---
+++ ClassAndObjectsBuildings/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassAndObjectsBuildings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Imobil imobil1 = new Imobil();
+
+            imobil1.tipCasa = "apartamanet";
+            imobil1.nrEtaje = 1;
+            imobil1.pret = 50000;
+            Console.WriteLine("Imobilul este: " + imobil1.tipCasa + "\nAre " + imobil1.nrEtaje + " etaje \nCosta: " + imobil1.pret);
+
+            Imobil imobil2 = new Imobil("casa");
+            imobil2.nrEtaje = 2;
+            imobil2.pret = 75000;
+            Console.WriteLine();
+            Console.WriteLine("Imobilul este: " + imobil2.tipCasa + "\nAre " + imobil2.nrEtaje + " etaje \nCosta: " + imobil2.pret);
+
+            Imobil imobil3 = new Imobil("bloc", 10, 10000000);
+            Console.WriteLine();
+            Console.WriteLine("Imobilul este: " + imobil3.tipCasa + "\nAre " + imobil3.nrEtaje + " etaje \nCosta: " + imobil3.pret);
+        }
+    }
+}
